cf1cc43420141d181de15dad944a3b7266ad6733
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-09 09:09:24 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Letter substuition to crack encoded message 9 Nov 19
+
+#object: change letter to letter 2 places further along in alphabet. For example,
+# a in encoded string becomes c in uncoded string
+# b in encoded string becomes d in uncoded string
+# c in encoded string becomes e in uncoded string
+# ....etc...
+# uncoded: a b c d e f g h i j k l m n o p q r s t u v w x y z
+# encoded: y z a b c d e f g h i j k l m n o p q r s t u v w x
+
+# The program will translate a coded string into a readable sentence.
+# g jmtc nwrfml = i love python
