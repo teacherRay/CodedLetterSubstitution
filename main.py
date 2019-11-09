611142e5fadc36e6478150fa514286af191a978a
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-09 09:33:40 +0700

    2nd Added dictionary

--- main.py
+++ main.py
@@ -10,3 +10,5 @@
 
 # The program will translate a coded string into a readable sentence.
 # g jmtc nwrfml = i love python
+encoded = {'a':'c', 'b':'d', 'c':'e', 'd':'f', 'e':'g', 'f':'h', 'g':'i', 'h':'j', 'i':'k', 'j':'l', 'k':'m', 'l':'n', 'm':'o', 'n':'p', 'o':'q', 'p':'r', 'q':'s', 'r':'t', 's':'u', 't':'v', 'u':'w', 'v':'x', 'w':'y', 'x':'z', 'y':'b' , 'z': 'c' }
+print(encoded['x']) # prints uncoded 'z'
